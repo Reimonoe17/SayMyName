5c41a3850b9f4412e95c55b698ada0d8422a33a7
Jamison Burton <jamison@burtontalk.com>
2022-01-19 08:16:54 -0700

    First draft of the code

---
+++ SayMyName/SayMyName.vb
@@ -0,0 +1,16 @@
+﻿Option Explicit On
+Option Strict On
+
+Module SayMyName
+
+    Sub Main()
+        Dim name As String 'defines a string variable
+        Console.WriteLine("Name: ") 'writes the prompt for the user to input name
+
+        name = Console.ReadLine() 'Sets the value input to the String variable "name"
+
+        Console.WriteLine("Hello " & name & ", my name is Tom Riddle. How did you get this diary?")
+        Console.ReadLine() 'Sends a response addressing the input string
+    End Sub
+
+End Module
